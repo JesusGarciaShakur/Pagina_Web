e7722b554d0ec1eee4b851d284d96612101499c1
JesusGarciaShakur <jesusrguez970@gmail.com>
2024-03-05 14:11:38 -0600

    First version CSS

--- page/websmca/static/js/script.js
+++ page/websmca/static/js/script.js
@@ -1,4 +1,18 @@
-function saluda (){
-    console.log("hello world!");
- }
-  saluda();
+document.addEventListener('DOMContentLoaded', function () {
+    const questions = document.querySelectorAll('.question');
+
+    questions.forEach(question => {
+        question.addEventListener('click', function () {
+            // Toggle de la clase 'active' en la pregunta para desplegar o contraer la respuesta
+            this.classList.toggle('active');
+
+            // Mostrar o ocultar la respuesta asociada
+            const answer = this.nextElementSibling;
+            if (answer.style.maxHeight) {
+                answer.style.maxHeight = null;
+            } else {
+                answer.style.maxHeight = answer.scrollHeight + 'px';
+            }
+        });
+    });
+});
